c0d62af1ed8be482eddc536bc5cd745606012ef2
Andra <andra.popa1989@gmail.com>
2019-01-26 18:46:02 +0000

    read input from arguments

--- input.py
+++ input.py
@@ -11,8 +11,18 @@
 
 
 
-age = input('How old are you? ')
-height = input('How tall are you? ')
-weight = input('How much do you weight? ')
+#age = input('How old are you? ')
+#height = input('How tall are you? ')
+#weight = input('How much do you weight? ')
+#
+#print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+from sys import argv
+
+script, first, second, third = argv
+
+print('The script is:', script)
+print('Your first variable is:', first)
+print('Your second variable is:', second)
+print('Your third variable is:', third)
