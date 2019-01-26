0b087e5b536522abee30314ce536e61e9c0b588a
Andra <andra.popa1989@gmail.com>
2019-01-26 17:58:59 +0000

    add tab

--- ex1.py
+++ ex1.py
@@ -14,16 +14,36 @@
 #    "Or a song about fear" 
 #))
       
-days = 'Mon Tue Wed Thu Fri Sat Sun'
+#days = 'Mon Tue Wed Thu Fri Sat Sun'
+#
+#months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug'
+#
+#print('Here are the days: ', days)
+#print('Here are the months: ', months)
+#
+#print("""
+#There's something going on here.
+#With the three doule-quotes.
+#We'll be albe to type as much as we like.
+#Even 4 lines if we want, or 5, or 6
+#""")
 
-months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug'
 
-print('Here are the days: ', days)
-print('Here are the months: ', months)
+# \t
 
-print("""
-There's something going on here.
-With the three doule-quotes.
-We'll be albe to type as much as we like.
-Even 4 lines if we want, or 5, or 6
-""")
+#fat_cat = """
+#    I'll do a list:
+#    \t* Cat food
+#    \t& Fishies
+#    \t* Catnip\n\t* Grass
+#"""
+#
+#print(fat_cat)
+
+
+# output: 
+#    I'll do a list:
+#        * Cat food
+#        & Fishies
+#        - Catnip
+#        * Grass
