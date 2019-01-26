65ea1ca17189ff49bbfbd3b4d192af02a9b2ae3a
Andra <andra.popa1989@gmail.com>
2019-01-26 17:49:43 +0000

    add format ex

---
+++ ex1.py
@@ -0,0 +1,29 @@
+#formatter = "{} {} {} {}"
+#
+#print(formatter.format(1, 2, 3, 4)) 
+#
+#print(formatter.format("one", "two", "three", "four")) 
+#print(formatter.format(True, False, False, True)) 
+#
+#print(formatter.format(formatter, formatter, formatter, formatter)) 
+#
+#print(formatter.format( 
+#    "Try your", 
+#    "Own text here", 
+#    "Maybe a poem", 
+#    "Or a song about fear" 
+#))
+      
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+
+months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug'
+
+print('Here are the days: ', days)
+print('Here are the months: ', months)
+
+print("""
+There's something going on here.
+With the three doule-quotes.
+We'll be albe to type as much as we like.
+Even 4 lines if we want, or 5, or 6
+""")
